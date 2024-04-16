7765ff4379656c1a54b9ecbbc4688d5ba34d9e57
Myondoesoho <MyondoesohoRoblox@gmail.com>
2024-04-16 10:24:53 -0400

    add readme

---
+++ Database/Fruits.sql
@@ -0,0 +1,8 @@
+CREATE TABLE fruit(
+    Ranking INTEGER PRIMARY KEY, fruit TEXT
+);
+INSERT INTO fruit VALUES (1, "Peaches");
+INSERT INTO fruit VALUES (2, "Grapes");
+INSERT INTO fruit VALUES (3, "Watermelon");
+INSERT INTO fruit VALUES (4, "Mango");
+
---
+++ README.md
@@ -0,0 +1,4 @@
+#Mostapha's Website
+
+Click [here] (git@github.com:Jonker233/MyBottleApps.git) for my bottble 
+apps repo.
